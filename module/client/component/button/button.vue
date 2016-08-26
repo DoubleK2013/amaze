91afc431600e1ceacecfce48f2aaa16e8bcf6aa9
DoubleK2013 <1037246890@qq.com>
2016-08-26 18:36:20 +0800

    add component

---
+++ module/client/component/button/button.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        { msg }
+    </div>
+</template>
+<style lang="less">
+
+</style>
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+
+        }
+    }
+</script>
